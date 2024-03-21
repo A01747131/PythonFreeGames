a7f2b1822dafabee58b4fb6b98c14901ca63938d
Ivan <a01747131@itesm.mx>
2024-03-21 09:45:14 -0600

    Added the yellow color on the P key

--- paint.py
+++ paint.py
@@ -38,7 +38,7 @@ def square(start, end):
 
 def circle(start, end):
     """Draw circle from start to end."""
-    pass  # TODO
+    pass  #TODO
 
 
 def rectangle(start, end):
@@ -79,6 +79,7 @@ onkey(lambda: color('white'), 'W')
 onkey(lambda: color('green'), 'G')
 onkey(lambda: color('blue'), 'B')
 onkey(lambda: color('red'), 'R')
+onkey(lambda: color('yellow'), 'P')
 onkey(lambda: store('shape', line), 'l')
 onkey(lambda: store('shape', square), 's')
 onkey(lambda: store('shape', circle), 'c')
